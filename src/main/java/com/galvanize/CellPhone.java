2fb661a0e6bc9917436f9c6b1224a9fca7918784
Mendez27 <edgarmendez2118@gmail.com>
2023-01-18 16:14:26 -0500

    Initiate part 2

--- src/main/java/com/galvanize/CellPhone.java
+++ src/main/java/com/galvanize/CellPhone.java
@@ -2,8 +2,35 @@ package com.galvanize;
 
 public class CellPhone
 {
+    boolean active = false;
+    CallingCard card;
+
     public CellPhone(CallingCard card)
     {
-
+        this.card = card;
     }
+
+    public int getRate()
+    {
+        return card.rate;
+    }
+
+    public void call(String number)
+    {
+        active = true;
+    }
+
+    public boolean isTalking()
+    {
+       return active;
+    }
+    public void tick()
+    {
+        card.balance--;
+    }
+    public void endCall()
+    {
+        active = false;
+    }
+
 }
--- src/test/java/com/galvanize/CellPhoneTest.java
+++ src/test/java/com/galvanize/CellPhoneTest.java
@@ -2,7 +2,7 @@ package com.galvanize;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CellPhoneTest
 {
@@ -12,13 +12,85 @@ public class CellPhoneTest
         //Setup
         CallingCard card = new CallingCard(20);
         CellPhone phone = new CellPhone(card);
+
+        CallingCard card2 = new CallingCard(10);
+        CellPhone phone2 = new CellPhone(card2);
         //Enact
-
+        int test = phone.getRate();
+        int test2 = phone2.getRate();
         //Assert
-        assertEquals();
-
+        assertEquals(20, test);
+        assertEquals(10, test2);
 
     }
+    @Test
+    public void verifyCellPhoneCanStartACall()
+    {
+        //Setup
+        CallingCard card = new CallingCard(20);
+        CellPhone phone = new CellPhone(card);
+
+        CallingCard card2 = new CallingCard(10);
+        CellPhone phone2 = new CellPhone(card2);
+        //Enact
+        phone.call("555-5555");
+        phone2.call("552-2121");
+
+
+        //Assert
+        assertTrue(phone.active);
+        assertTrue(phone2.active);
+
+    }
+    @Test
+    public void verifyCanCheckThereIsAnActiveCall()
+    {
+        //Setup
+        CallingCard card = new CallingCard(20);
+        CellPhone phone = new CellPhone(card);
+
+        CallingCard card2 = new CallingCard(10);
+        CellPhone phone2 = new CellPhone(card2);
+
+        //Enact
+        phone.call("555-5555");
+
+        //Assert
+        assertTrue(phone.isTalking());
+        assertFalse(phone2.isTalking());
+    }
+    @Test
+    public void verifyMinutesCanTickFromACellPhone()
+    {
+        //Setup
+        CallingCard card = new CallingCard(20);
+        card.addDollars(1);
+        CellPhone phone = new CellPhone(card);
+
+        //Enact
+        phone.tick();
+        int test = card.getRemainingMinutes();
+        phone.tick();
+        int toast = card.getRemainingMinutes();
+        //Assert
+        assertEquals(4,test);
+        assertEquals(3,toast);
+    }
+    @Test
+    public void verifyCellPhoneCanEndACall()
+    {
+        //Setup
+        CallingCard card = new CallingCard(20);
+        CellPhone phone = new CellPhone(card);
+        phone.call("555-5555");
+
+        //Enact
+        phone.endCall();
+        boolean test = phone.isTalking();
+        //Assert
+        assertFalse(test);
+    }
+
 }
 //Setup
 //Enact
